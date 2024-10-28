c9b4809013a7d2817bfa773bbc827896a119a232
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-10-27 22:29:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests 
-import base64 
+import base64
+import io
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
